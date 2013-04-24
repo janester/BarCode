3bf81680f09fbc025e1c527d4655eb95525ba173
Jane Sternbach <janesternbach@gmail.com>
2013-04-24 15:34:42 -0400

    production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,8 @@ Barcode::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :ses
 end
