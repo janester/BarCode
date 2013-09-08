4b0316bb071c4c628ac55173c66fd126a6ae3672
Jane Sternbach <janesternbach@gmail.com>
2013-09-08 03:47:07 -0400

    some styling and error-proofing

--- app/controllers/pub_crawls_controller.rb
+++ app/controllers/pub_crawls_controller.rb
@@ -6,39 +6,26 @@ class PubCrawlsController < ApplicationController
   end
   def new
     @crawl = PubCrawl.new
-    @times = []
-    25.times do |hour|
-      if hour < 10
-        a = "0#{hour}:00"
-      else
-        a = "#{hour}:00"
-      end
-      @times << [a,a]
-      @end_times = []
-    end
+    @end_times = []
+    @times = PubCrawl.create_times
   end
 
   def end_times
     start = params[:start_time][0..1].to_i
-    @end_times = []
-    (25-start).times do |hour|
-      if hour+start < 10
-        a = "0#{hour+start}:00"
-      else
-        a = "#{hour+start}:00"
-      end
-      @end_times << [a,a]
-    end
+    @end_times = PubCrawl.create_end_times(start)
   end
 
   def create
-    p = PubCrawl.create(params[:pub_crawl])
-    p.users << @cu
-    p.start_time = params[:start_time]
-    p.end_time = params[:end_times]
-    p.save
-    @crawl = p
-    Notifications.made_crawl_message(@cu, @crawl).deliver
+    @crawl = PubCrawl.new(params[:pub_crawl])
+    @crawl.start_time = params[:start_time]
+    @crawl.end_time = params[:end_times]
+    if @crawl.save
+      @crawl.users << @cu
+      Notifications.made_crawl_message(@cu, @crawl).deliver
+    else
+      @end_times = []
+      @times = PubCrawl.create_times
+    end
   end
 
   def join_crawl
--- app/models/pub_crawl.rb
+++ app/models/pub_crawl.rb
@@ -15,9 +15,31 @@ class PubCrawl < ActiveRecord::Base
   attr_accessible :date, :end_time, :name, :start_time
   has_and_belongs_to_many :users
   has_many :stops
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 
 
   def venues
     self.stops.map(&:venue)
+  end
+
+  def self.create_times
+    times = []
+    25.times do |hour|
+      hour < 10 ? a = "0#{hour}:00" : a = "#{hour}:00"
+      times << [a,a]
+    end
+    return times
+  end
+
+  def self.create_end_times(start)
+    end_times = []
+    (25-start).times do |hour|
+      hour+start < 10 ? a = "0#{hour+start}:00" : a = "#{hour+start}:00"
+      end_times << [a,a]
+    end
+    return end_times
   end
 end
